1ebe31bc4cdef15c8f366668900aac2c558c7898
cahityusuf <56351404+cahityusuf@users.noreply.github.com>
2020-04-19 02:01:57 +0300

    first commit

---
+++ Tema/Controllers/UyapOlaylariController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tema.Controllers
+{
+    public class UyapOlaylariController : Controller
+    {
+        public IActionResult OlayGirisIslemleri()
+        {
+            return View();
+        } 
+        public IActionResult ihbarGirisIslemleri()
+        {
+            return View();
+        }
+        public IActionResult RaporOnayIslemleri()
+        {
+            return View();
+        }
+        public IActionResult OlayOnayIslemleri()
+        {
+            return View();
+        }public IActionResult BelgeIslemleri()
+        {
+            return View();
+        }
+    }
+}
